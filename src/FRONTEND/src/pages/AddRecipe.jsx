5085a1e0804bf245c89160f9fd9954ab0584a1f4
murad <goshakip1278@gmail.com>
2023-11-05 11:39:42 +0100

    add_recipe fix final commit before deadline

--- src/FRONTEND/src/pages/AddRecipe.jsx
+++ src/FRONTEND/src/pages/AddRecipe.jsx
@@ -8,45 +8,45 @@ function AddRecipe () {
         <div className="container">
             <h1>Add Recipe</h1>
             <form action="#" method="POST" encType="multipart/form-data">
-                <label htmlFor="name">Name:</label>
+                <label htmlFor="name">Name</label>
                 <input type="text" id="name" name="name" required />
 
-                <label htmlFor="description">Description:</label>
-                <textarea id="description" name="description" rows="4" required></textarea>
+                <label htmlFor="description">Description</label>
+                <input type="text" id="description" name="description" required></input>
 
-                <label htmlFor="notes">Notes:</label>
-                <textarea id="notes" name="notes" rows="4"></textarea>
+                <label htmlFor="notes">Notes</label>
+                <input type="text" id="notes" name="notes"></input>
 
-                <label htmlFor="tags">Tags (comma-separated):</label>
+                <label htmlFor="tags">Tags</label>
                 <input type="text" id="tags" name="tags" />
 
-                <label htmlFor="photo">Photo:</label>
+                <label htmlFor="photo">Photo</label>
                 <input type="file" id="photo" name="photo" accept="image/*" />
 
-                <label htmlFor="ingredient">Ingredient:</label>
+                <label htmlFor="ingredient">Ingredients</label>
                 <input type="text" id="ingredient" name="ingredient" />
 
-                <label htmlFor="rating">Rating:</label>
+                <label htmlFor="rating">Rating</label>
                 <input type="number" id="rating" name="rating" min="1" max="5"/>
 
-                <label htmlFor="difficulty">Difficulty:</label>
+                <label htmlFor="difficulty">Difficulty</label>
                 <select id="difficulty" name="difficulty">
                     <option value="easy">Easy</option>
                     <option value="medium">Medium</option>
                     <option value="hard">Hard</option>
                 </select>
 
-                <label htmlFor="prep-time">Prep Time (minutes):</label>
+                <label htmlFor="prep-time">Prep time (minutes)</label>
                 <input type="number" id="prep-time" name="prep-time" />
 
-                <label htmlFor="cook-time">Cook Time (minutes):</label>
+                <label htmlFor="cook-time">Cook time (minutes)</label>
                 <input type="number" id="cook-time" name="cook-time" />
 
-                <label htmlFor="portions">Amount of Portions:</label>
+                <label htmlFor="portions">Amount of portions</label>
                 <input type="number" id="portions" name="portions" />
 
-                <label htmlFor="tips">Tips:</label>
-                <textarea id="tips" name="tips" rows="4"></textarea>
+                <label htmlFor="tips">Tips</label>
+                <input type="text" id="tips" name="tips"></input>
 
                 <button className="button" type="submit">Submit Recipe</button>
             </form>
