b639765a94dcb0b8c72d1d678fd164ed5c027312
murad <goshakip1278@gmail.com>
2023-11-07 13:53:07 +0100

    connected fe and be (for 1 function only)

--- src/FRONTEND/src/pages/MyRecipes.jsx
+++ src/FRONTEND/src/pages/MyRecipes.jsx
@@ -1,42 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function MyRecipes() {
-    const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const apiUrl = 'http://localhost:8000/api/add-recipes/'; // Replace with your API URL
 
-    // Simulated recipe data (you can replace this with actual data)
-    const initialRecipes = [
-        {
-            id: 1,
-            name: 'Spaghetti Carbonara',
-            description: 'Classic Italian pasta dish',
-        },
-        {
-            id: 2,
-            name: 'Chicken Stir-Fry',
-            description: 'Delicious stir-fried chicken and vegetables',
-        },
-    ];
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setRecipes(data);
+        } else {
+          console.error('API response is not an array:', data);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, [apiUrl]);
 
-    useEffect(() => {
-        // Simulated fetching of user's recipes (replace with actual API call)
-        setRecipes(initialRecipes);
-    }, []);
-
-    return (
-        <div className="my-recipes">
-            <h1>My Recipes</h1>
-            <ul className="recipe-list">
-                {recipes.map((recipe) => (
-                    <li className="recipe-item" key={recipe.id}>
-                        <h2>{recipe.name}</h2>
-                        <p>{recipe.description}</p>
-                    </li>
-                ))}
-            </ul>
-            <Link to="/">Back to Main Page</Link>
-        </div>
-    );
+  return (
+    <div className="my-recipes">
+      <h1>My Recipes</h1>
+      <ul className="recipe-list">
+        {recipes.map((recipe) => (
+          <li className="recipe-item" key={recipe.id}>
+            <h2>{recipe.name}</h2>
+            <p>{recipe.description}</p>
+          </li>
+        ))}
+      </ul>
+      <Link to="/">Back to Main Page</Link>
+    </div>
+  );
 }
 
 export default MyRecipes;
